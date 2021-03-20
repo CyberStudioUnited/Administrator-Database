bdad4f15ff9c8ee95c4e816aad6a998c6c24c137
Soup Foundry <72953518+CyberStudioUnited@users.noreply.github.com>
2021-03-20 00:39:20 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<title>admin database port 127.0.0.1</title>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: "Lato", sans-serif;
+}
+
+.sidenav {
+  height: 100%;
+  width: 195px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #696969;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+.sidenav a {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 25px;
+  color: black;
+  display: block;
+}
+
+.sidenav a:hover {
+  color: #f1f1f1;
+}
+
+.main {
+  margin-left: 200px; /* Same as the width of the sidenav */
+  font-size: 28px; /* Increased text to enable scrolling */
+  padding: 0px 10px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
+</style>
+</head>
+<body>
+
+<div class="sidenav">
+  <img src="ADMINDB.png" alt="AdminDB">
+  <a href="settings.php">Settings</a>
+  <a href="index.php">Tools</a>
+</div>
+
+
+<div class="main">
+  <h2>Administrator Database</h2>
+  <p>Welcome to the website manager for you.</p>
+  <p>All Tools.</p>
+  <img src="fm.png" alt="FM" href="filemanager.php" width="100" height="100" ><a href="filemanager.php">File Manager</a>
+  <img src="domain.png" alt="FM" href="dc.php" width="100" height="100" ><a href="dc.php">Domain Checker</a>
+  <img src="cookies.png" alt="FM" href="cookies.php" width="100" height="100" ><a href="cookies.php">Cookies Checker</a>
+  <P></p>
+  <img src="buildwebsite.png" alt="FM" href="buildwebsite.php" width="100" height="100" ><a href="buildwebsite.php">Website Builder</a>
+</div>
+   
+</body>
+</html> 
